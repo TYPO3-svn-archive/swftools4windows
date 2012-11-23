1a752d332d8df6ddf0839654ceac30ac70b773b8
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-23 14:02:08 +0000

    0.0.1 - Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/swftools4windows/trunk@68001 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "pdfcontroller".
+#
+# Auto generated 21-02-2012 13:31
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Swftools for Windows',
+	'description' => 'swftools.exe: Install swftools on a Windows server. You must be root!',
+	'category' => 'backend',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'dependencies' => '',
+	'conflicts' => '',
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
